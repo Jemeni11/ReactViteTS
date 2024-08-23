a0c78f8c07a1ca1de10dae866549ced1b3bf6a94
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-08-23 09:57:32 +0100

    chore: Update Vite and React versions, add Vite logo and update landing page title

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div className='flex place-content-center'>
+    <>
+      <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className='text-3xl font-bold underline'>Vite + React + TS + Tailwind + React Router + Redux Toolkit</h1>
+      <h1>
+        ReactViteTS (Vite + React + TypeScript + TailwindCSS + Prettier +
+        ESLint)
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -27,8 +31,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
