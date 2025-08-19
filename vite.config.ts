529f6c788b84c187262c222acbecfa34b71d6b05
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-08-19 17:24:04 +0100

    build: add path alias support for "@/src/*" in TS and Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 
 import tailwindcss from "@tailwindcss/vite";
@@ -6,4 +7,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
